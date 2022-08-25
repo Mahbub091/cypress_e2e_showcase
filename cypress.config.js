6f5cfb9cf67fc8abcc61995fa07e4712610de0da
Md. Mahbubur Rahman <mahbubasr091@gmail.com>
2022-08-26 04:45:14 +0600

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  defaultCommandTimeout: 20000,
-  execTimeout: 60000,
-  pageLoadTimeout: 60000,
+  defaultCommandTimeout: 12000,
+  execTimeout: 20000,
+  pageLoadTimeout: 20000,
   requestTimeout: 15000,
   responseTimeout: 15000,
   video: false,
