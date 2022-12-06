d2e576d704d43ec78e5807e67b8959521e8f32a5
unknown <talhabinashraf@gmail.com>
2022-12-06 20:20:23 +0600

    Test4 added

---
+++ cypress/tests/Test-4-BrokenImageValidation.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+import homepage from '../pages/homePage';
+import baseFunc from '../pages/functions';
+
+import credUtils from '../support/credentialUtils';
+
+describe('Incorrect Login Validation', () => {
+  it('Navigating to the website', () => {
+    cy.visit('/');
+  });
+
+  it('Homepage navigation verification', () => {
+    cy.validUrl('exercise', credUtils.homePage);
+    baseFunc.ValidatePageTitle('Automation Exercise');
+  });
+
+});
